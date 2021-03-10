4a22165350c898047c6d263ba47c5643837c326d
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-03-10 11:04:56 +0500

    Delete самая_длинная строка.py

--- hw_3/самая_длинная строка.py
+++
@@ -1,9 +0,0 @@
-a = (input())
-b = (input())
-c = (input())
-if len(a) < len(c) and len(b) < len(c):
-    print(c)
-if len(c) < len(a) and len(b) < len(a):
-    print(a)
-if len(a) < len(b) and len(c) < len(b):
-    print(b)
