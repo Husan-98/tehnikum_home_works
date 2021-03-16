3ac3f0096ca03a500a377db2bd8a4c39add64b77
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-03-16 16:32:19 +0500

    Create самое_длинное_слово_в_строке.py

---
+++ hw2/самое_длинное_слово_в_строке.py
@@ -0,0 +1,13 @@
+a = 'asdsgdfg qwrrterwq'
+a = a.split(' ')
+
+b = []
+for i in a:
+    b.append(len(i))
+
+b = max(b)
+for i in a:
+    if b == len(i):
+        print(i)
+    else:
+        continue
