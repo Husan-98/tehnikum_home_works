36e3b2aacdbeb1e7d87be8b2b9eecc75125a5e00
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-03-16 15:32:01 +0500

    Update №2.py

--- hw1/№2.py
+++ hw1/№2.py
@@ -1,5 +1,21 @@
+#поиск наибольшего числа 
+
 a = float(input())
 b = float(input())
 c = float(input())
 average = (a + b + c) - max(a, b, c) - min(a, b, c)
 print((a + b + c) - average - min(a, b, c))
+
+######################################################
+#второй вариант 
+a = float(input())
+b = float(input())
+c = float(input())
+m = 0
+if a >= m:
+    m = a
+if b > m:
+    m = b
+if c > m:
+    m = c
+print(m)
