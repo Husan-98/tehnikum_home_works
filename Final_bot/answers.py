de5a8d2761252e10c3c190e6ee8625538b35d3ba
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-04-08 20:11:08 +0500

    Create answers.py

---
+++ Final_bot/answers.py
@@ -0,0 +1,10 @@
+import messege as msg
+
+eng_answers = {'Hello': msg.en_hello,
+               'Bye': msg.en_bye}
+
+rus_answers = {'да': msg.ru_hello,
+               'пока': msg.ru_bye}
+
+global_answers = {'rus': rus_answers,
+                  'eng': eng_answers}
