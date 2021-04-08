f74da93f2d701447c25b8f5694d6ba0f8f85a69c
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-04-08 20:07:38 +0500

    Create start.py

---
+++ Final_bot/start.py
@@ -0,0 +1,22 @@
+"""
+Модуль для старта проекта
+"""
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+
+
+from constant import API_TOKEN
+from function import *
+
+
+
+updater = Updater(token=API_TOKEN)
+
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler("start", start))
+
+dispatcher.add_handler(MessageHandler(Filters.text, text_msg))
+
+dispatcher.add_handler(CallbackQueryHandler(callback=inline_reply, pattern='yes'))
+
+updater.start_polling()
